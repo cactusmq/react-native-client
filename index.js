9110b1c56ddf1a6026c12c65ad5c52328c0a7466
David J Barnes <nullableint@gmail.com>
2018-10-10 16:48:55 -0600

    refactored export

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
 import RNEventSource from "react-native-event-source";
 
-var request = require("request");
 var ApiURL = "http://api.cactusmq.com/v1/";
 
-function CactusMQ(options) {
-    this.publishURL = ApiURL.concat("publish/", options.publishKey);
-    this.subscribeURL = ApiURL.concat("subscribe/", options.subscribeKey);
-    this.onMessage = options.onMessage;
-    this.onPublish = options.onPublish;
-    this.onError = options.onError;
-};
+export default class CactusMQ {
+    constructor(options) {
+        this.publishURL = ApiURL.concat("publish/", options.publishKey);
+        this.subscribeURL = ApiURL.concat("subscribe/", options.subscribeKey);
+        this.onMessage = options.onMessage;
+        this.onPublish = options.onPublish;
+        this.onError = options.onError;
+    }
 
-CactusMQ.prototype.publish = function (topic, message, data) { 
-    fetch(this.publishURL, {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json', 'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            topic: topic,
-            message: message,
-            data: data
-        })
-        .then((response) => response.json())
-        .then((response) => {
-            this.onPublish(response);
-        })
-        .catch((error) => {
-            this.onError(error);
-        })
-    })
-};
-
-CactusMQ.prototype.subscribe = function (topic) { 
-    var eventSource = new EventSource(this.subscribeURL);
-    eventSource.addEventListener(topic, e => {
-        this.onMessage(e);
-    });
-};
-
-module.exports = CactusMQ;
+    publish(topic, message, data) {
+        fetch(this.cactusPublish, {
+            method: "POST",
+            headers: {
+                Accept: "application/json", "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                topic: topic,
+                message: message,
+                data: data
+            })
+                .then((response) => response.json())
+                .then((response) => {
+                    this.onPublish(response);
+                }).catch(() => {
+                    this.onError(error);
+            })
+        });
+    }
+   
+    subscribe(topic) {
+        var eventSource = new EventSource(this.subscribeURL);
+        eventSource.addEventListener(topic, event => {
+            this.onMessage(event);
+        }); 
+    }
+ }
